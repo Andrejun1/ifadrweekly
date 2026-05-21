58bc3772aa5a769d037a6cb65d3998e7408fa6b9
andrejun1 <andrejunika06@gmail.com>
2026-05-21 20:00:52 +0700

    update project from laragon

---
+++ proses.php
@@ -0,0 +1,15 @@
+<?php
+
+$nama = $_POST['nama'];
+$uts = $_POST['uts'];
+$uas = $_POST['uas'];
+$tugas = $_POST['tugas'];
+
+echo "<h2>Data Mahasiswa</h2>";
+
+echo "Nama: $nama <br>";
+echo "UTS: $uts <br>";
+echo "UAS: $uas <br>";
+echo "Tugas: $tugas <br>";
+
+?>
